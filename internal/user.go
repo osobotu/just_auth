14d8d86bd8aa2df5a4fde997f8f150010d431c14
Stephen Obotu Oduh <osobotu@gmail.com>
2024-04-11 23:55:54 +0100

    feat(setup): add makefile and workflow for tests

--- internal/user.go
+++ internal/user.go
@@ -1,1 +1,11 @@
 package just_auth
+
+import "github.com/google/uuid"
+
+type User struct {
+	ID           uuid.UUID
+	FirstName    string
+	LastName     string
+	Email        string
+	PasswordHash string
+}
